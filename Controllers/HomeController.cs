6be98d21b2aa34660b0fe477a9402ce747ccddca
Tillman Pugh <Till.pugh@gmail.com>
2023-06-08 15:58:20 -0700

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurvey.Models;
+
+namespace DojoSurvey.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+
+    [HttpPost("addStudent")]
+    public IActionResult AddStudent(string Name, string Location, string Language, string Comment) 
+    {
+        Console.WriteLine($"{Name} {Location} {Language} {Comment}");
+        ViewBag.Name = Name;
+        ViewBag.Location = Location;
+        ViewBag.Language = Language;
+        ViewBag.Comment = Comment;
+        return View("Results");
+    }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
