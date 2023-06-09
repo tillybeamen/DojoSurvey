fc840633d19de192a306dbdd3bf7c9751ed83266
Tillman Pugh <Till.pugh@gmail.com>
2023-06-09 16:37:33 -0700

    third commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,54 +1,4 @@
 ﻿﻿#pragma warning disable CS8618
-// using System.Diagnostics;
-// using Microsoft.AspNetCore.Mvc;
-// using DojoSurvey.Models;
-
-// namespace DojoSurvey.Controllers;
-// public class HomeController : Controller
-// {
-//     static Survey student;
-//     private readonly ILogger<HomeController> _logger;
-
-//     public HomeController(ILogger<HomeController> logger)
-//     {
-//         _logger = logger;
-//     }
-
-//     [HttpGet("")]
-//     public IActionResult Index()
-//     {
-//         return View();
-//     }
-
-
-//     [HttpPost("addStudent")]
-//     public IActionResult AddStudent(Survey student) 
-//     {
-//         System.Console.WriteLine(student.Name);
-//         System.Console.WriteLine(student.Location);
-//         System.Console.WriteLine(student.Language);
-//         System.Console.WriteLine(student.Comment);
-//         // ViewBag.Name = Name;
-//         // ViewBag.Location = Location;
-//         // ViewBag.Language = Language;
-//         // ViewBag.Comment = Comment;
-
-//         // To replace ViewBag with View model try this
-//         // int MyNumber = 7;
-//         // return View(MyNumber)
-//         return View("Results");
-//     }
-//     public IActionResult Privacy()
-//     {
-//         return View();
-//     }
-
-//     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-//     public IActionResult Error()
-//     {
-//         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-//     }
-// }
 
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
@@ -78,7 +28,15 @@ public class HomeController : Controller
     public IActionResult Process(Survey user)
     {
         NewUser = user;
-        return RedirectToAction("Results");
+        if(ModelState.IsValid)
+        {
+            return RedirectToAction("Results");
+        }
+        else
+        {
+        return View("Index");
+
+        }
     }
 
     [HttpGet("results")]
--- Models/Survey.cs
+++ Models/Survey.cs
@@ -1,9 +1,15 @@
 #pragma warning disable CS8618
+using System.ComponentModel.DataAnnotations;
 namespace DojoSurvey.Models;
 public class Survey
 {
+    [Required]
+    [MinLength(2)]
     public string Name {get; set;}
+    [Required]
     public string Location {get; set;}
+    [Required]
     public string Language {get; set;}
+    [MinLength(20)]
     public string? Comment {get; set;}
 }
